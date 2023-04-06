4aeb17171e99eaf5ba3dda57e965503d292c7435
Leandro <leandrodev83@gmail.com>
2023-04-06 14:27:21 -0300

    Criando o formulário de login e o banco de dados com a  tabela usuarios

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="custom.css">
+</head>
+
+<body>
+    <div class="login">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 offset-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3>Acesso Restrito</h3>
+                        </div>
+                        <div class="card-body">
+                            <form action="login.php" method="POST">
+                                <div class="mb-3">
+                                    <label>Usuário</label>
+                                    <input type="text" name="usuario" class="form-control">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label>Senha</label>
+                                    <input type="password" name="senha" class="form-control">
+                                </div>
+
+                                <div class="mb-3">
+                                    <button type="submit" class="btn btn-primary">Enviar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
